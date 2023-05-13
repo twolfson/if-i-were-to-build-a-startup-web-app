2d34fc46edba5c04ddf0118267de9bc0d3434ca4
Todd Wolfson <todd@twolfson.com>
2023-05-13 05:14:44 -0700

    Realized form_valid was good

--- examples/django-limited-ajax-react-tailwind/myapp/views.py
+++ examples/django-limited-ajax-react-tailwind/myapp/views.py
@@ -3,16 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import ProcessFormView
+from django.views.generic import FormView
 
 @login_required
 def index(request):
     return render(request, "index.html")
 
-# TODO: Class view for methods views instead of same one
 # https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-editing/#django.views.generic.edit.FormView
-# ProcessFormView is a better fit due to no success_url requirement/ordering, https://github.com/django/django/blob/4.2.1/django/views/generic/edit.py#L139
-class SignUpFormView(ProcessFormView):
+class SignUpFormView(FormView):
     template_name = "registration/sign_up.html"
     form_class = UserCreationForm
 
@@ -20,5 +18,6 @@ class SignUpFormView(ProcessFormView):
         username = form.cleaned_data.get('username')
         raw_password = form.cleaned_data.get('password1')
         user = authenticate(username=username, password=raw_password)
-        login(request, user)
+        login(self.request, user)
+        # DEV: Don't call form_valid() as it'll want to use a success URL, https://github.com/django/django/blob/4.2.1/django/views/generic/edit.py#L63-L65
         return redirect('index')
