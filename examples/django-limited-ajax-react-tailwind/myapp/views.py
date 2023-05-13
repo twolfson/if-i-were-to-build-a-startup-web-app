557ebe25effa4fb7a1dd3e5f7338a2e424a46a86
Todd Wolfson <todd@twolfson.com>
2023-05-13 03:52:34 -0700

    Added sane redirect logic

--- examples/django-limited-ajax-react-tailwind/myapp/views.py
+++ examples/django-limited-ajax-react-tailwind/myapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-
-# TODO: Move to templates, duh
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the myapp index.")
+    # TODO: Redirect abstraction prob is widely applied as a mixin, no?
+    if not request.user.is_authenticated:
+        return redirect("/login")
+
+    # TODO: Use a `render` template instead
+    return HttpResponse(f"Hello {request.user.first_name}")
