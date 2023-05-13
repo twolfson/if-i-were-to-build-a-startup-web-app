56f7ef61ccadd12af7f9f618460d0f0bff4336e1
Todd Wolfson <todd@twolfson.com>
2023-05-13 04:13:20 -0700

    Got HTML loading

--- examples/django-limited-ajax-react-tailwind/myapp/views.py
+++ examples/django-limited-ajax-react-tailwind/myapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 def index(request):
     # TODO: Redirect abstraction prob is widely applied as a mixin, no?
     if not request.user.is_authenticated:
+        # TODO: If not abstracted, use `url 'login'`
         return redirect("/login")
 
     # TODO: Use a `render` template instead
