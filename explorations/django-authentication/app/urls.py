ced5c16c1eccf73fd9585a9f2811d186c554b914
Todd Wolfson <todd@twolfson.com>
2023-05-13 16:05:30 -0700

    Added new URLs

--- explorations/django-authentication/app/urls.py
+++ explorations/django-authentication/app/urls.py
@@ -4,10 +4,33 @@ from . import views
 
 urlpatterns = [
     # Provide multiple auth login by default, https://docs.djangoproject.com/en/4.2/topics/auth/default/#using-the-views
-    # DEV: Technically auth isn't required for demo purposes, but we think this highlights Django's strengths
-    path("", include("django.contrib.auth.urls")),
+    # https://github.com/django/django/blob/4.2.1/django/contrib/auth/urls.py
+    # TODO: Update to login explicitly, https://github.com/django/django/blob/4.2.1/django/contrib/auth/forms.py#L199-L210
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path("password_change/", views.PasswordChangeView.as_view(), name="password_change"),
+    path(
+        "password_change/done/",
+        views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
     # Django doesn't have a sign up view, so use this guide: https://simpleisbetterthancomplex.com/tutorial/2017/02/18/how-to-create-user-sign-up-view.html
     path("sign-up/", views.SignUpFormView.as_view(), name="sign_up"),
-
     path("", views.index, name="index"),
 ]
