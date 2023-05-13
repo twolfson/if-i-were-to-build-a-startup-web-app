2d6fe83fc5c430955f245598e46c4b7e352423a6
Todd Wolfson <todd@twolfson.com>
2023-05-13 05:22:23 -0700

    Spining wheels a lot on views apparently... Then again, my Django forms experience is limited

--- examples/django-limited-ajax-react-tailwind/myapp/views.py
+++ examples/django-limited-ajax-react-tailwind/myapp/views.py
@@ -3,18 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.views.generic import FormView
+from django.views.generic.edit import CreateView
 
 @login_required
 def index(request):
     return render(request, "index.html")
 
-# https://docs.djangoproject.com/en/4.2/ref/class-based-views/generic-editing/#django.views.generic.edit.FormView
-class SignUpFormView(FormView):
+class SignUpFormView(CreateView):
     template_name = "registration/sign_up.html"
     form_class = UserCreationForm
 
     def form_valid(self, form):
+        # Don't call `super().form_valid()` as it tries to redirect
+        #   https://github.com/django/django/blob/4.2.1/django/views/generic/edit.py#L133-L136
+        #   https://github.com/django/django/blob/4.2.1/django/views/generic/edit.py#L63-L65
+        form.save()
+
         username = form.cleaned_data.get('username')
         raw_password = form.cleaned_data.get('password1')
         user = authenticate(username=username, password=raw_password)
