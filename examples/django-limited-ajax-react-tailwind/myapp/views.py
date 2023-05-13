3352bb2d6c3f0732addb13b0944bf86f7eb96b1e
Todd Wolfson <todd@twolfson.com>
2023-05-13 04:15:43 -0700

    Found the login_required decorator, as expected

--- examples/django-limited-ajax-react-tailwind/myapp/views.py
+++ examples/django-limited-ajax-react-tailwind/myapp/views.py
@@ -1,11 +1,7 @@
-from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
+@login_required
 def index(request):
-    # TODO: Redirect abstraction prob is widely applied as a mixin, no?
-    if not request.user.is_authenticated:
-        # TODO: If not abstracted, use `url 'login'`
-        return redirect("/login")
-
     # TODO: Use a `render` template instead
     return HttpResponse(f"Hello {request.user.first_name}")
--- examples/django-limited-ajax-react-tailwind/mysite/settings.py
+++ examples/django-limited-ajax-react-tailwind/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Auth settings
+#   https://docs.djangoproject.com/en/4.2/topics/auth/default/#django.contrib.auth.decorators.login_required
+#   https://docs.djangoproject.com/en/4.2/ref/settings/#login-url
+LOGIN_URL = "/login/"
