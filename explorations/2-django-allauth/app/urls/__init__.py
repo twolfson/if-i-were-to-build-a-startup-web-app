9c69dff439a4bf8037cd89f59d35f116c9bb2f89
Todd Wolfson <todd@twolfson.com>
2023-05-14 13:06:11 -0700

    Moved back to login at top-level

--- explorations/2-django-allauth/app/urls/__init__.py
+++ explorations/2-django-allauth/app/urls/__init__.py
@@ -3,7 +3,9 @@ from django.urls import include, path
 from app import views
 
 urlpatterns = [
-    path("account/", include("app.urls.auth.account")),
+    # Technically everything is configured to serve at `/accounts/` (e.g. `/accounts/login`))
+    #   but UX-wise, stating `/accounts/` in URL just feels confusing =/ (esp if we introduce a model named Account)
+    path("", include("app.urls.auth.account")),
     # Social login disabled currently
     # path("socialaccount/", include("app.urls.auth.socialaccount")),
     path("", views.index, name="index"),
