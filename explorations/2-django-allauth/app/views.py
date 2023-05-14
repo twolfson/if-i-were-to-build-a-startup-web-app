fab7de950d0cbadf5b69b7a858a1e6b6e2427f95
Todd Wolfson <todd@twolfson.com>
2023-05-14 14:14:34 -0700

    Updating messages and errors

--- explorations/2-django-allauth/app/views.py
+++ explorations/2-django-allauth/app/views.py
@@ -1,7 +1,14 @@
 from allauth.account.decorators import verified_email_required
+from django.contrib import messages
 from django.shortcuts import render
 
 
 @verified_email_required
 def index(request):
+    return render(request, "index.html")
+
+
+@verified_email_required
+def message_test(request):
+    messages.info(request, "Hello World!")
     return render(request, "index.html")
