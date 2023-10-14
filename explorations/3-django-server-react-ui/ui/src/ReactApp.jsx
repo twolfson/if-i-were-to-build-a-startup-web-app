7b466221d63a1b934dd6d0be200fda3cbbe9af88
Todd Wolfson <todd@twolfson.com>
2023-10-14 12:02:00 -0700

    Implemented form errors

--- explorations/3-django-server-react-ui/ui/src/ReactApp.jsx
+++ explorations/3-django-server-react-ui/ui/src/ReactApp.jsx
@@ -6,6 +6,6 @@ export const ReactApp = () => {
   const user = window.user;
   const isLoggedIn = !user.is_anonymous;
 
-  // TODO: Handle routing (sign up) and 404 pages
+  // DEV: This is missing routing (e.g. sign up, 404 pages)
   return <>{isLoggedIn ? `Hello ${user.full_name}` : <Login />}</>;
 };
--- explorations/3-django-server-react-ui/ui/src/pages/Login.jsx
+++ explorations/3-django-server-react-ui/ui/src/pages/Login.jsx
@@ -1,16 +1,30 @@
 // Based on: https://github.com/pennersr/django-allauth/blob/0.54.0/allauth/templates/account/login.html
 // Based on: explorations/2-django-allauth/app/templates/account/login.html
-// TODO: It'd be nice to use `head_title: Login`
-// TODO: Handle form errors
 import { AuthLayout } from "./_layouts/AuthLayout";
 import { Checkbox } from "../components/Checkbox";
 import { CsrfToken } from "../components/CsrfToken";
 import { Input } from "../components/Input";
 
 export const Login = () => {
+  // DEV: It'd be nice to use `head_title: Login`
+  // DEV: It'd prob be more efficient to define a context which looks for form errors
+
+  // Process our form errors into a common array for top of form
+  // DEV: We always render a `json_script` for each of these in this exploration, even on page load
+  // DEV: We're using `_` to avoid confusing with `combinedFormErrors`
+  const _formErrors = JSON.parse(document.querySelector("head > #form__errors").innerText);
+  const _formNonFieldErrors = JSON.parse(document.querySelector("head > #form__non_field_errors").innerText);
+  // __all__ appears as `_formNonFieldErrors` as well (e.g. email+password incorrect)
+  delete _formErrors["__all__"];
+  const formErrorsAsArray = Object.entries(_formErrors).map(([key, val]) =>
+    `${key}: ${val}`
+  )
+  const combinedFormErrors = formErrorsAsArray.concat(_formNonFieldErrors);
+
   return (
     <AuthLayout>
       <h1 className="mb-3">Login</h1>
+      { combinedFormErrors.length ? <p class="text-danger">{combinedFormErrors.join(" ")}</p> : null }
       {/* Fields enumerated here: https://github.com/pennersr/django-allauth/blob/0.54.0/allauth/account/forms.py#L90 */}
       {/* DEV: `form` submits as an HTML form, not an XHR submission */}
       <form action="/login/" method="post" className="mb-4">
