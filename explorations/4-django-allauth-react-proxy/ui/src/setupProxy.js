8f39c5233e23dd443d6f965b2c6ee725ae82d539
Todd Wolfson <todd@twolfson.com>
2023-10-20 09:12:21 -0700

    Added clarifying comment

--- explorations/4-django-allauth-react-proxy/ui/src/setupProxy.js
+++ explorations/4-django-allauth-react-proxy/ui/src/setupProxy.js
@@ -1,8 +1,15 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
+// Configuration via https://create-react-app.dev/docs/proxying-api-requests-in-development/
+// DEV: We cannot use the default `proxy` setting since it disallows `Allow: text/html` (e.g. browser navigation)
 module.exports = function (app) {
   app.use(
-    "/auth/accounts",
+    // Route multiple paths to Django, https://stackoverflow.com/a/26058349/1960509
+    [
+      "/admin",
+      "/api",
+      "/auth/accounts",
+    ],
     createProxyMiddleware({
       target: "http://localhost:8000",
       changeOrigin: true,
