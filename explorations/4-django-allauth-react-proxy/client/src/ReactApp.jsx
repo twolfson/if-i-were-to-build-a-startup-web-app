0f0b438c8a0aef1a954eded7693eafe3c89a42a5
Todd Wolfson <todd@twolfson.com>
2023-10-21 18:39:54 -0700

    Migrated back to setups which make sense since we control rendering element during navigation

--- explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
+++ explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
@@ -12,11 +12,40 @@ import { Index } from "./pages/Index";
 const LOGGED_IN_KEY = "logged_in";
 const LOGGED_IN_SUCCESS_VALUE = "1";
 
+// DEV: We use a component instead of a `loader`, because otherwise React Router tries to render content regardless
+const AuthRequired = ({ children }) => {
+  // DEV: Use `useState` so we cache initial value (assumes always logged in) and doesn't touch `localStorage` until later
+  //   Caching as `false` doesn't matter because the page will redirect to login
+  const [isLoggedIn, ] = useState(
+    window.localStorage[LOGGED_IN_KEY] === LOGGED_IN_SUCCESS_VALUE
+  );
+
+  if (!isLoggedIn) {
+    // TODO: Note in README about missing URL redirect support on login
+    // DEV: We can't use `redirect` since that uses `history.pushState`
+    window.location = "/auth/login/";
+    return null;
+  }
+  return children;
+};
+
+
+// TODO: Relocate to proper place
+const LogoutPage = () => {
+  // Only unset localStorage once at load
+  useEffectOnce(() => {
+    delete window.localStorage[LOGGED_IN_KEY];
+  });
+
+  // DEV: We can't use `redirect` since that uses `history.pushState`
+  window.location = "/auth/logout/";
+  return null;
+}
 
 const router = createBrowserRouter([
   {
     path: "/auth-success",
-    action: () => {
+    loader: () => {
       // DEV: If we ever need to put this back into a component, then use `useEffectOnce`
       window.localStorage[LOGGED_IN_KEY] = LOGGED_IN_SUCCESS_VALUE;
 
@@ -26,29 +55,14 @@ const router = createBrowserRouter([
   },
   {
     path: "/logout",
-    loader: () => {
-      // DEV: If we ever need to put this back into a component, then use `useEffectOnce`
-      delete window.localStorage[LOGGED_IN_KEY];
-      // DEV: We can't use `redirect` since that uses `history.pushState`
-      window.location = "/auth/logout/";
-      return null
-    },
-    element: null,
+    element: <LogoutPage />,
   },
   {
     path: "/",
-    loader: () => {
-      // DEV: If we ever need to put this back into a component, then use `useState` to cache
-      const isLoggedIn = window.localStorage[LOGGED_IN_KEY] === LOGGED_IN_SUCCESS_VALUE;
-      if (!isLoggedIn) {
-        // TODO: Note in README about missing URL redirect support on login
-        // DEV: We can't use `redirect` since that uses `history.pushState`
-        window.location = "/auth/login/";
-        return false;
-      }
-    },
     element: (
-      <Index />
+      <AuthRequired>
+        <Index />
+      </AuthRequired>
     ),
   },
 ]);
