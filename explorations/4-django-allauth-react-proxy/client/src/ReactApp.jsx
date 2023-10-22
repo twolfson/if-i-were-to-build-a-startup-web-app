fed86315a5bac09a84b96e1a61a0e5f1ebe7012f
Todd Wolfson <todd@twolfson.com>
2023-10-21 22:29:44 -0700

    Reworking to just not use router for auth handling

--- explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
+++ explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
@@ -1,36 +1,40 @@
 // Import our dependencies
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useCookie } from "react-use";
 
-import { AuthRequiredContainer } from "./containers/AuthRequiredContainer";
-import { MessagesContainer } from "./containers/MessagesContainer";
 import { AuthSuccessLoader } from "./loaders/AuthSuccessLoader";
 import { Index } from "./pages/Index";
+import { LOGGED_IN_COOKIE_NAME } from "./utils/constants";
 
 const router = createBrowserRouter([
   {
+    // This handles the completion side of auth login (acts as a callback)
     path: "/auth-success",
     loader: AuthSuccessLoader,
   },
   {
     path: "/",
-    element: <AuthRequiredContainer />,
-    children: [
-      {
-        index: true,
-        // DEV: Required as part of Django auth process, to avoid weird messages at logout
-        element: <MessagesContainer />,
-        children: [
-          {
-            index: true,
-            element: <Index />,
-          },
-        ],
-      },
-    ],
+    element: <Index />,
   },
 ]);
 
 // Define our component
 export const ReactApp = () => {
+  // Determine our login status
+  // DEV: Use `useState` so we cache initial value (assumes always logged in) and doesn't touch `localStorage` until later
+  //   Caching as `false` doesn't matter because the page will redirect to login
+  const [isLoggedIn] = useCookie(LOGGED_IN_COOKIE_NAME);
+
+  // TODO: Load up messages with `isLoggedIn` as request conditional
+
+  // If we're not logged in, navigate to Django's auth pages
+  if (!isLoggedIn) {
+    // TODO: Note in README about missing URL redirect support on login
+    // DEV: We can't use `redirect` since that uses `history.pushState`
+    window.location = "/auth/login/";
+    return null;
+  }
+
+  // Otherwise, perform routing as per normal
   return <RouterProvider router={router} />;
 };
