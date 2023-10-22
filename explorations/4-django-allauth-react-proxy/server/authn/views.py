b8d146b9d3fb1daecd4b3d024671bcf76e234146
Todd Wolfson <todd@twolfson.com>
2023-10-22 09:43:27 -0700

    Completed rough structure

--- explorations/4-django-allauth-react-proxy/server/authn/views.py
+++ explorations/4-django-allauth-react-proxy/server/authn/views.py
@@ -1,17 +1,19 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.contrib.messages import get_messages
 
 
 @require_http_methods(["GET"])
 def messages(request):
     # https://github.com/jgerigmeyer/jquery-django-messages-ui/blob/2.0.2/messages_ui/middleware.py#L45-L55
+    # https://docs.djangoproject.com/en/4.2/ref/contrib/messages/#displaying-messages
     message_list = [
         {
+            "message": message.message,
             "level": message.level,
-            "message": text_type(message.message),
             "tags": message.tags,
         }
-        for message in messages.get_messages(request)
+        for message in get_messages(request)
     ]
 
     return JsonResponse(message_list)
