ed9183926435701494ad31645c4b694f600eac8a
Todd Wolfson <todd@twolfson.com>
2023-10-21 18:10:23 -0700

    Rough auth-success works

--- explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
+++ explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
@@ -1,6 +1,8 @@
 // Import our dependencies
 import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useEffectOnce } from "react-use";
+
 import { Index } from "./pages/Index";
 
 const LOGGED_IN_KEY = "logged_in";
@@ -8,28 +10,34 @@ const LOGGED_IN_SUCCESS_VALUE = "1";
 
 const AuthRequired = ({ children }) => {
   // DEV: Use `useState` so we cache initial value (assumes always logged in) and doesn't touch `localStorage` until later
+  //   Caching as `false` doesn't matter because the page will redirect to login
   // TODO: Handle actually changing state on auth success
   const [isLoggedIn, setIsLoggedIn] = useState(
     window.localStorage[LOGGED_IN_KEY] === LOGGED_IN_SUCCESS_VALUE
   );
 
   if (!isLoggedIn) {
-    return "TODO: Redirect";
+    return "TODO: Redirect to auth URL with current URL as query param";
   }
   return children;
 };
 
+// TODO: Relocate to proper place
 const AuthSuccessPage = () => {
-  // TODO: Handle redirect
+  // Only set localStorage once at load
+  // DEV: Due to this being separate/before `AuthRequired`, we won't have hit the cache there yet
   useEffectOnce(() => {
+    window.localStorage[LOGGED_IN_KEY] = LOGGED_IN_SUCCESS_VALUE;
+  });
 
-  })
+  return "TODO: Redirect to provided URL or /"
+  // TODO: Handle redirect
 }
 
 const router = createBrowserRouter([
   {
     path: "/auth-success",
-    element: <div>TODO</div>,
+    element: <AuthSuccessPage />,
   },
   {
     path: "/",
