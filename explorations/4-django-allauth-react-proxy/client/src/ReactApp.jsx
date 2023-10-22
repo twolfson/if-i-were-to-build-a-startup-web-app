90615c0892d9924dd5d484b5efd5d07c9c8bf5a9
Todd Wolfson <todd@twolfson.com>
2023-10-21 18:26:31 -0700

    Reworked to redirect code but it's not working =/

--- explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
+++ explorations/4-django-allauth-react-proxy/client/src/ReactApp.jsx
@@ -1,6 +1,10 @@
 // Import our dependencies
 import { useState } from "react";
-import { createBrowserRouter, RouterProvider, redirect } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  redirect,
+} from "react-router-dom";
 import { useEffectOnce } from "react-use";
 
 import { Index } from "./pages/Index";
@@ -8,57 +12,39 @@ import { Index } from "./pages/Index";
 const LOGGED_IN_KEY = "logged_in";
 const LOGGED_IN_SUCCESS_VALUE = "1";
 
-const AuthRequired = ({ children }) => {
-  // DEV: Use `useState` so we cache initial value (assumes always logged in) and doesn't touch `localStorage` until later
-  //   Caching as `false` doesn't matter because the page will redirect to login
-  const [isLoggedIn, ] = useState(
-    window.localStorage[LOGGED_IN_KEY] === LOGGED_IN_SUCCESS_VALUE
-  );
-
-  if (!isLoggedIn) {
-    // TODO: Note in README about missing URL redirect support on login
-    return redirect("/auth/login/")
-  }
-  return children;
-};
-
-// TODO: Relocate to proper place
-const AuthSuccessPage = () => {
-  // Only set localStorage once at load
-  // DEV: Due to this being separate/before `AuthRequired`, we won't have hit the cache there yet
-  useEffectOnce(() => {
-    window.localStorage[LOGGED_IN_KEY] = LOGGED_IN_SUCCESS_VALUE;
-  });
-
-  // TODO: Note in README about missing URL redirect support on login
-  return redirect("/")
-}
-
-// TODO: Relocate to proper place
-const LogoutPage = () => {
-  // Only unset localStorage once at load
-  useEffectOnce(() => {
-    delete window.localStorage[LOGGED_IN_KEY];
-  });
-
-  return redirect("/auth/logout/")
-}
 
 const router = createBrowserRouter([
   {
     path: "/auth-success",
-    element: <AuthSuccessPage />,
+    action: () => {
+      // DEV: If we ever need to put this back into a component, then use `useEffectOnce`
+      window.localStorage[LOGGED_IN_KEY] = LOGGED_IN_SUCCESS_VALUE;
+
+      // TODO: Note in README about missing URL redirect support on login
+      return redirect("/");
+    },
   },
   {
     path: "/logout",
-    element: <LogoutPage />,
+    action: () => {
+      // DEV: If we ever need to put this back into a component, then use `useEffectOnce`
+      delete window.localStorage[LOGGED_IN_KEY];
+      return redirect("/auth/logout/");
+    },
+    element: null,
   },
   {
     path: "/",
+    loader: () => {
+      // DEV: If we ever need to put this back into a component, then use `useState` to cache
+      const isLoggedIn = window.localStorage[LOGGED_IN_KEY] === LOGGED_IN_SUCCESS_VALUE;
+      if (!isLoggedIn) {
+        // TODO: Note in README about missing URL redirect support on login
+        return redirect("/auth/login/");
+      }
+    },
     element: (
-      <AuthRequired>
-        <Index />
-      </AuthRequired>
+      <Index />
     ),
   },
 ]);
