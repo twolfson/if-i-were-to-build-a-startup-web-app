69264b2b5100efa9a0515db54fe756bd648a02d8
Todd Wolfson <todd@twolfson.com>
2023-10-21 20:35:51 -0700

    Re-enabled social pages

--- explorations/4-django-allauth-react-proxy/server/authn/urls/__init__.py
+++ explorations/4-django-allauth-react-proxy/server/authn/urls/__init__.py
@@ -5,6 +5,5 @@ urlpatterns = [
     # Technically everything is configured to serve at `/accounts/` (e.g. `/accounts/login`))
     #   but UX-wise, stating `/accounts/` in URL just feels confusing =/ (esp if we introduce a model named Account)
     path("", include("authn.urls.account")),
-    # Social login disabled currently
-    # path("social/", include("authn.urls.socialaccount")),
+    path("social/", include("authn.urls.socialaccount")),
 ]
